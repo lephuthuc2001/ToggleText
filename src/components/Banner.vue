e23807e0291183aa8d8ecc2c89992cecd55ea44f
Le Phu Thuc <lephuthuc2001@gmail.com>
2024-05-21 21:00:22 +0700

    feat: :sparkles: Create Banner Component to test Slot Features

---
+++ src/components/Banner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="banner flex-grow gap-2 p-4 flex flex-col content-start max-w-96 border rounded border-orange-500 border-solid"
+  >
+    <section>
+      <slot name="header"> Header </slot>
+    </section>
+    <hr />
+    <section>
+      <slot name="content" :discount="50"> Content </slot>
+    </section>
+    <hr />
+
+    <section v-if="$slots.footer">
+      <slot name="footer"> Footer </slot>
+    </section>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,12 +11,59 @@
         eius, maxime iste?
       </ToggleText>
     </div>
+
+    <hr />
+    <h2 class="text-2xl">Test SLOTS</h2>
+    <div class="flex gap-4 flex-wrap">
+      <Banner />
+      <Banner>
+        <template #header>
+          <h2 class="text-2xl">Welcome to Our Summer Sale!</h2>
+        </template>
+        <template #content>
+          <p>Enjoy up to 50% off on all items.</p>
+        </template>
+        <template #footer>
+          <button
+            class="mx-auto w-fit border border-gray-300 rounded-md px-2 py-1 bg-orange-600 text-white"
+          >
+            Learn more
+          </button>
+        </template>
+      </Banner>
+      <Banner>
+        <template #header>
+          <h2 class="text-2xl">Welcome to Our Summer Sale!</h2>
+        </template>
+        <template #content="{ discount }">
+          <p>
+            Enjoy up to
+            <strong class="text-orange-600">{{ discount }}%</strong> off on all
+            items.
+          </p>
+        </template>
+      </Banner>
+      <Banner>
+        <template #header>
+          <h2 class="text-2xl">Welcome to Our Summer Sale!</h2>
+        </template>
+
+        <template #content="{ discount }" :discount="discount">
+          <strong class="text-orange-600">{{ discount }}%</strong> off is NICE
+        </template>
+      </Banner>
+    </div>
   </main>
 </template>
 
 <script setup>
 import ToggleText from "./ToggleText.vue";
 import ComposableExample from "./ComposableExample.vue";
+import Banner from "./Banner.vue";
+import { ref, computed } from "vue";
+
+const discountPercent = 50;
+const discount = computed(() => `-${discountPercent}%`);
 </script>
 
 <style scoped></style>
