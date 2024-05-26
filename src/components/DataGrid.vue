54d6ccd43fa9cf2a4b4fb27887971911f1b56e64
Le Phu Thuc <lephuthuc2001@gmail.com>
2024-05-26 19:50:08 +0700

    feat: :sparkles: Added Skeleton for DataGrid

---
+++ src/components/DataGrid.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="flex gap-2 justify-end items-baseline mb-4">
+      <form>
+        <input
+          id="search"
+          type="text"
+          class="border border-slate-600 p-2 rounded"
+          placeholder="Search..."
+        />
+      </form>
+    </div>
+    <table
+      role="grid"
+      class="table-fixed border-collapse border-spacing-4 border border-black w-full"
+    >
+      <caption class="caption-bottom">
+        Table Caption
+      </caption>
+      <thead>
+        <tr>
+          <th class="border border-slate-600 p-4">Column 1</th>
+          <th class="border border-slate-600 p-4">Column 2</th>
+          <th class="border border-slate-600 p-4">Column 3</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="border border-slate-700 p-4">Row 1, Column 1</td>
+          <td class="border border-slate-700 p-4">Row 1, Column 2</td>
+          <td class="border border-slate-700 p-4">Row 1, Column 3</td>
+        </tr>
+        <tr>
+          <td class="border border-slate-700 p-4">Row 2, Column 1</td>
+          <td class="border border-slate-700 p-4">Row 2, Column 2</td>
+          <td class="border border-slate-700 p-4">Row 2, Column 3</td>
+        </tr>
+        <tr>
+          <td class="border border-slate-700 p-4">Row 3, Column 1</td>
+          <td class="border border-slate-700 p-4">Row 3, Column 2</td>
+          <td class="border border-slate-700 p-4">Row 3, Column 3</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td class="border border-slate-700 p-4" colspan="3">Footer</td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,75 +1,11 @@
 <template>
   <main class="flex flex-col gap-4">
-    <section>
-      <h2 class="mb-4 text-xl">Toggle Text component</h2>
-      <ToggleText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-        obcaecati quis quae. Quaerat saepe, reiciendis ut rerum corrupti
-        distinctio amet cupiditate ratione voluptatem est asperiores sint vero
-        eius, maxime iste?
-      </ToggleText>
-    </section>
-
-    <hr />
-    <ComposableExample />
-    <hr />
-    <h2 class="text-xl">Test SLOTS</h2>
-    <section class="flex gap-4 flex-wrap">
-      <Banner />
-      <Banner>
-        <template #header>
-          <h2 class="text-2xl">Welcome to Our Summer Sale!</h2>
-        </template>
-        <template #content>
-          <p>Enjoy up to 50% off on all items.</p>
-        </template>
-        <template #footer>
-          <button
-            class="mx-auto w-fit border border-gray-300 rounded-md px-2 py-1 bg-orange-600 text-white"
-          >
-            Learn more
-          </button>
-        </template>
-      </Banner>
-      <Banner>
-        <template #header>
-          <h2 class="text-2xl">Welcome to Our Summer Sale!</h2>
-        </template>
-        <template #content="{ discount }">
-          <p>
-            Enjoy up to
-            <strong class="text-orange-600">{{ discount }}%</strong> off on all
-            items.
-          </p>
-        </template>
-      </Banner>
-      <Banner>
-        <template #header>
-          <h2 class="text-2xl">Welcome to Our Summer Sale!</h2>
-        </template>
-
-        <template #content="{ discount }" :discount="discount">
-          <strong class="text-orange-600">{{ discount }}%</strong> off is NICE
-        </template>
-      </Banner>
-    </section>
-    <hr />
-    <AnimationTechniquesExample />
-    <hr />
-    <InputBindingTest />
+    <DataGrid />
   </main>
 </template>
 
 <script setup>
-import ToggleText from "./ToggleText.vue";
-import ComposableExample from "./ComposableExample.vue";
-import Banner from "./Banner.vue";
-import AnimationTechniquesExample from "./AnimationTechniquesExample.vue";
-import InputBindingTest from "./InputBindingTest.vue";
-import { ref, computed } from "vue";
-
-const discountPercent = 50;
-const discount = computed(() => `-${discountPercent}%`);
+import DataGrid from "./DataGrid.vue";
 </script>
 
 <style scoped></style>
