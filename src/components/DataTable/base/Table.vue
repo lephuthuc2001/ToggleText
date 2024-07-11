016756f1c75e4e37220ebe140c707824cd20a00f
Le Phu Thuc <lephuthuc2001@gmail.com>
2024-07-11 22:55:09 +0700

    style: :lipstick: Added Style to title

--- src/components/DataTable/base/Table.vue
+++ src/components/DataTable/base/Table.vue
@@ -10,17 +10,26 @@ const { items, columns, sortState } = defineProps({
 
 <template>
   <div class="overflow-y-auto">
-    <table>
-      <thead class="sticky-top">
+    <table
+      class="min-w-full leading-normal shadow-lg rounded-lg overflow-hidden"
+    >
+      <thead class="bg-gray-900 text-white">
         <tr>
-          <th v-for="column in columns" :key="column.key">
+          <th
+            v-for="column in columns"
+            :key="column.key"
+            class="px-6 py-4 border-b-2 border-gray-300 text-left text-sm uppercase tracking-wider"
+          >
             <template v-if="!column.sortable || sortState === undefined">
               <slot :name="'header-' + column.key" :value="column.label">
                 {{ column.label }}
               </slot>
             </template>
             <template v-else-if="column.sortable && sortState !== undefined">
-              <button @click="sortState.handleSort(column.key)">
+              <button
+                @click="sortState.handleSort(column.key)"
+                class="flex items-center space-x-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
                 <slot
                   :name="'header-' + column.key"
                   :value="column.label"
@@ -29,25 +38,35 @@ const { items, columns, sortState } = defineProps({
                     sortOrder: sortState.getSortOrder(column.key),
                   }"
                 >
-                  <span> {{ column.label }} </span>
-
-                  <span v-if="sortState.getSortOrder(column.key) === 'asc'"
+                  <span class="font-semibold"> {{ column.label }} </span>
+                  <span
+                    v-if="sortState.getSortOrder(column.key) === 'asc'"
+                    class="text-green-400"
                     >↑</span
                   >
                   <span
                     v-else-if="sortState.getSortOrder(column.key) === 'desc'"
-                    >↓
-                  </span>
-                  <span v-else>↕</span>
+                    class="text-red-400"
+                    >↓</span
+                  >
+                  <span v-else class="text-gray-400">↕</span>
                 </slot>
               </button>
             </template>
           </th>
         </tr>
       </thead>
-      <tbody>
-        <tr v-for="item in items" :key="item.id">
-          <td v-for="column in columns" :key="column.key">
+      <tbody class="bg-white divide-y divide-gray-200">
+        <tr
+          v-for="item in items"
+          :key="item.id"
+          class="transition duration-150 ease-in-out hover:bg-gray-50"
+        >
+          <td
+            v-for="column in columns"
+            :key="column.key"
+            class="px-6 py-3 text-sm"
+          >
             <slot :name="'body-' + column.key" :value="column.getValue(item)">
               {{ column.getValue(item) }}
             </slot>
