fecc2c02158556cedfcd4ae970ff80813d335864
Le Phu Thuc <lephuthuc2001@gmail.com>
2024-08-01 23:55:49 +0700

    feat: :sparkles: Added Custom error-message for dateOfBirth

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -381,7 +381,12 @@ import { useForm, useFieldArray, FieldArray } from "vee-validate";
 import SignaturePad from "signature_pad";
 import { useTranslation } from "i18next-vue";
 import { setLocale } from "yup";
-import { set } from "lodash";
+import {
+  phoneNumberRegex,
+  isOlderThan18,
+  isSkillDetail,
+  getLastPartOfString,
+} from "../utils/utils";
 
 const { t, i18next } = useTranslation();
 
@@ -432,35 +437,6 @@ const initialValues = {
   ],
   agreement: false,
   signature: "",
-};
-
-const phoneNumberRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g;
-
-const isOlderThan18 = (value) => {
-  const { day, month, year } = value;
-
-  // Convert day, month, and year to integers
-  const dayInt = parseInt(day, 10);
-  const monthInt = parseInt(month, 10);
-  const yearInt = parseInt(year, 10);
-
-  if (!dayInt || !monthInt || !yearInt) {
-    return true; // Skip validation if any of the fields are missing or invalid
-  }
-
-  const today = new Date();
-  const birthDate = new Date(yearInt, monthInt - 1, dayInt); // month is 0-indexed
-  let age = today.getFullYear() - birthDate.getFullYear();
-  const monthDifference = today.getMonth() - birthDate.getMonth();
-
-  if (
-    monthDifference < 0 ||
-    (monthDifference === 0 && today.getDate() < birthDate.getDate())
-  ) {
-    age--;
-  }
-
-  return age >= 18;
 };
 
 i18next.on("languageChanged", () => {
@@ -561,23 +537,6 @@ const { handleSubmit, defineField, errors, setFieldValue, resetForm } = useForm(
   }
 );
 
-function isSkillDetail(str) {
-  const regex = /^skills\[\d+\]\.(level|name)$/;
-  const match = regex.exec(str);
-  if (match) {
-    return {
-      isMatch: true,
-      isName: match[1] === "name",
-      isLevel: match[1] === "level",
-    };
-  }
-  return {
-    isMatch: false,
-    isName: false,
-    isLevel: false,
-  };
-}
-
 const localizedErrors = computed(function () {
   const output = {};
 
@@ -634,7 +593,7 @@ const localizedErrors = computed(function () {
 
       output[key] = message;
     } else {
-      const message = t("validation.custom." + key, {
+      const message = t("validation.custom." + getLastPartOfString(key), {
         ns: "applicationForm",
       });
       output[key] = message;
---
+++ src/utils/utils.js
@@ -0,0 +1,50 @@
+export const phoneNumberRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g;
+
+export const isOlderThan18 = (value) => {
+  const { day, month, year } = value;
+
+  // Convert day, month, and year to integers
+  const dayInt = parseInt(day, 10);
+  const monthInt = parseInt(month, 10);
+  const yearInt = parseInt(year, 10);
+
+  if (!dayInt || !monthInt || !yearInt) {
+    return true; // Skip validation if any of the fields are missing or invalid
+  }
+
+  const today = new Date();
+  const birthDate = new Date(yearInt, monthInt - 1, dayInt); // month is 0-indexed
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDifference = today.getMonth() - birthDate.getMonth();
+
+  if (
+    monthDifference < 0 ||
+    (monthDifference === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age >= 18;
+};
+
+export function isSkillDetail(str) {
+  const regex = /^skills\[\d+\]\.(level|name)$/;
+  const match = regex.exec(str);
+  if (match) {
+    return {
+      isMatch: true,
+      isName: match[1] === "name",
+      isLevel: match[1] === "level",
+    };
+  }
+  return {
+    isMatch: false,
+    isName: false,
+    isLevel: false,
+  };
+}
+
+export function getLastPartOfString(str) {
+  const parts = str.split(".");
+  return parts[parts.length - 1];
+}
